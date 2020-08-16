22e15c0acf8a93795848afad0fd71a61be709a22
Eduardo Briguenti Vieira <eduardo.vieira@bestseller.com>
2020-08-16 11:04:49 +0200

    hackerrank maximumDraws

---
+++ src/main/java/com/edubriguenti/problems/hackerrank/MaximumDraws.java
@@ -0,0 +1,15 @@
+package com.edubriguenti.problems.hackerrank;
+
+public class MaximumDraws {
+
+    static int maximumDraws(int n) {
+        return n + 1;
+    }
+
+    public static void main(String[] args) {
+        //Testing
+        assert maximumDraws(1) == 2 : "Expected 2";
+        assert maximumDraws(2) == 3 : "Expected 3";
+        assert maximumDraws(3) == 4 : "Expected 4";
+    }
+}
